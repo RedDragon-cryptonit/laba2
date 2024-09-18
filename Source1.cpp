9ea2c5f5a71852572226b1f22ac8d0499f8e218e
NMM <nmm@mail.ru>
2024-09-18 18:37:17 +0700

    исправил ошибки

--- Source1.cpp
+++ Source1.cpp
@@ -6,29 +6,28 @@
 
 using namespace std;
 
-int main()
-{
+int main(){
 
   double a = 5.54;
   double b = 3.93;
   double y = 1.45;
   float K;
-  float n_1;
-  float n_2;
-  float n_3;
+  float n1;
+  float n2;
+  float n3;
   float c;
-  int T_1 = 288;
-  int T_2 = 675;
+  int T1 = 288;
+  int T2 = 675;
 
   K = (y - 1) / y;
-  n_1 = 1 - (pow((1 / b), K));
-  n_2 = (T_2 - T_1) / (T_2 + ((T_2 - T_1) / ((y - 1) * log(a))));
-  n_3 = 1 - K * (log(b) / (pow(b, K) - 1));
+  n1 = 1 - (pow((1 / b), K));
+  n2 = (T_2 - T_1) / (T_2 + ((T_2 - T_1) / ((y - 1) * log(a))));
+  n3 = 1 - K * (log(b) / (pow(b, K) - 1));
 
   cout << "K = " << K << endl;
-  cout << "n1 = " << n_1 << endl;
-  cout << "n2 = " << n_2 << endl;
-  cout << "n3 = " << n_3 << endl;
+  cout << "n1 = " << n1 << endl;
+  cout << "n2 = " << n2 << endl;
+  cout << "n3 = " << n3 << endl;
 
   return 0;
 }
